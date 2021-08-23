abd34e5ba7f2a4384c1cdc6d1a1f4390b9797389
Curar <58891522+Curar@users.noreply.github.com>
2021-08-23 17:08:09 +0200

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+//  DNS lookups
+//  writing for fun ✈
+//  by Curar 2021 ☠
+//  Writing on Linux in the Vim editor
+
+use dns_lookup::lookup_host;
+use std::io;
+use std::io::Write;
+use std::thread;
+use std::time;
+
+fn main() {
+   
+    println!("DNS lookups\n\n\n");
+
+    loop {
+    
+    let mut adres=String::new();
+    
+    print!("Please enter a valid domain (google.com) : ");
+
+    match io::stdout().flush() {
+        Ok(_) => print!("") ,
+        Err(e) => println!("{}", e),
+        }
+
+    match io::stdin().read_line(&mut adres) {
+        Ok(_) => {
+            adres = adres.trim().to_string();
+                if adres.len() != 0 {
+
+                    if let Some('\n')=adres.chars().next_back() {
+    
+                        adres.pop();
+    
+                    }
+                    if let Some('\r')=adres.chars().next_back() {
+
+                        adres.pop();
+    
+                    }
+    
+                 println!("You typed domain : {}", adres);
+    
+                
+                let ips: Vec<std::net::IpAddr> = lookup_host(&adres).unwrap();
+                
+                println!("\n{:?}", ips);
+    
+                thread::sleep(time::Duration::from_millis(500));
+
+                println!("\n\nPress (Ctrl + C) to exit . . .");
+
+
+              } else {
+                println!("Attention! Please enter a valid domain");
+               }
+        }
+        Err(error) => println!("{}", error),
+    }
+  }
+}
